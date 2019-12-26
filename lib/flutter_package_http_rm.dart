8814a814a93df5cb89bc16f55c558fc495f23da0
xie <xieyang01@finupgroup.com>
2019-12-26 17:16:06 +0800

    [xie]添加引入头文件

--- lib/flutter_package_http_rm.dart
+++ lib/flutter_package_http_rm.dart
@@ -1,5 +1,8 @@
 library flutter_package_http_rm;
 
+export 'package:flutter_package_http_rm/scr/http_rm_configuration.dart';
+export 'package:flutter_package_http_rm/scr/flutter_package_http_rm.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
