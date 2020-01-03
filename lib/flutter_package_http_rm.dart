6fe17a3541dcf0fe2494e1d4dd13ed8b920a81f2
xie <xieyang01@finupgroup.com>
2020-01-03 14:53:46 +0800

    [xie] 外露错误类

--- lib/flutter_package_http_rm.dart
+++ lib/flutter_package_http_rm.dart
@@ -4,6 +4,7 @@ export 'package:flutter_package_http_rm/src/http_rm_configuration.dart';
 export 'package:flutter_package_http_rm/src/flutter_package_http_rm.dart';
 export 'package:flutter_package_http_rm/src/http_rm_options.dart';
 export 'package:flutter_package_http_rm/src/http_rm_result_data.dart';
+export 'package:flutter_package_http_rm/src/http_rm_error.dart';
 export 'package:dio/dio.dart';
 /// A Calculator.
 class Calculator {
